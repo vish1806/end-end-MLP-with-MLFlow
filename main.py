1639d8f1a478d7204a5af2bafc221c25730fc5a3
vish1806 <vishalganpathy@gmail.com>
2024-03-21 21:55:53 +0530

    import error

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from mlProject import logger
-from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
