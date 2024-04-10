74005614d23cd4e26be7c61b0bfc40ad048ae9f8
vish1806 <vishalganpathy@gmail.com>
2024-04-10 10:48:46 +0530

    model trainer added

--- src/mlProject/config/configuration.py
+++ src/mlProject/config/configuration.py
@@ -1,6 +1,7 @@
 from mlProject.constants import *
 from mlProject.utils.common import *
-from src.mlProject.entity.config_entity import (DataIngestionConfig, DataValidationConfig)
+from src.mlProject.entity.config_entity import (DataIngestionConfig, DataTransformationConfig, DataValidationConfig, ModelTrainerConfig)
+
 class ConfigurationManager:
     def __init__(self, config_filepath = CONFIG_FILE_PATH, params_filepath = PARAMS_FILE_PATH, schema_filepath = SCHEMA_FILE_PATH):
         self.config = read_yaml(config_filepath)
@@ -41,3 +42,34 @@ class ConfigurationManager:
         return data_validation_config
     
 
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        config = self.config.data_transformation
+
+        create_directories([config.root_dir])
+
+        data_transformation_config = DataTransformationConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+        )
+
+        return data_transformation_config
+    
+    def get_model_trainer_config(self) -> ModelTrainerConfig:
+        config = self.config.model_trainer
+        params = self.params.ElasticNet
+        schema =  self.schema.TARGET_COLUMN
+
+        create_directories([config.root_dir])
+
+        model_trainer_config = ModelTrainerConfig(
+            root_dir=config.root_dir,
+            train_data_path = config.train_data_path,
+            test_data_path = config.test_data_path,
+            model_name = config.model_name,
+            alpha = params.alpha,
+            l1_ratio = params.l1_ratio,
+            target_column = schema.name
+            
+        )
+
+        return model_trainer_config
